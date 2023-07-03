d17820a4541b35affe0a107c5219312df4eeb109
Hyunjoon Choi <90085154+devholic22@users.noreply.github.com>
2023-07-03 19:12:41 +0900

    feat: Shop 엔티티 및 MemberAndShop 정의
    - Shop 엔티티 생성
    - MemberAndShop 엔티티 생성
    - 필요한 리포지토리 생성

---
+++ src/main/java/umc/reco/repository/ShopRepository.java
@@ -0,0 +1,7 @@
+package umc.reco.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import umc.reco.entity.Shop;
+
+public interface ShopRepository extends JpaRepository<Shop, Long> {
+}
