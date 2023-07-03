a7a30f23df4a3f9a89fd2b10f4c829af8ef56602
Hanbee Lee <bee1162@naver.com>
2023-07-04 00:08:29 +0900

    feat:총Point 처리

--- src/main/java/umc/reco/service/ShopService.java
+++ src/main/java/umc/reco/service/ShopService.java
@@ -91,15 +91,26 @@ public class ShopService {
         Member loggedInMember = userUtil.getLoggedInMember();
         Tree tree = treeService.getTreeByMember(loggedInMember);
 
+        double updatedTotalPoint = countPoint(tree);
+
         double totalMl = tree.getTotal_ml();
         double mlToAdd = mlDto.getMl();
         double updatedTotalMl = totalMl + mlToAdd;
 
         tree.setTotal_ml(updatedTotalMl);
+        tree.setPoint(updatedTotalPoint);
         MemberAndShop memberAndShop = memberAndShopRepository.findByMemberIdAndShopId(loggedInMember.getId(), findShop.getId())
                 .orElseGet(() -> createMemberAndShop(loggedInMember, findShop));
         memberAndShop.setMl(memberAndShop.getMl() + mlDto.getMl());
         return new MemberAndShopResponseDto(loggedInMember.getEmail(), findShop.getName(), memberAndShop.getHeart(),
                 memberAndShop.getMl());
+    }
+
+
+    private double countPoint(Tree tree){
+        double totalPoint = tree.getPoint();
+        double updatedTotalMl = totalPoint +2000;
+
+        return updatedTotalMl;
     }
 }
